39dbfb78fef38f44751ccb7981eedc5e9935573c
Fernando <fernandolm@gmail.com>
2022-03-14 23:00:05 -0300

    fix: made code run in leet code website
    change return value

--- src/main/java/com/example/demo/problems/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/example/demo/problems/RemoveDuplicatesFromSortedArray.java
@@ -34,7 +34,7 @@ public class RemoveDuplicatesFromSortedArray implements Test {
         Printer.printEnd(this.getClass().getName());
     }
 
-    private int[] removeDuplicates(int[] duplicatedArray, int[] answerArray, MutableBoolean result) {
+    private int removeDuplicates(int[] duplicatedArray, int[] answerArray, MutableBoolean result) {
         Printer.print(MessageFormat.format("Duplicated Array: {0} | Answer Array: {1}", Arrays.toString(duplicatedArray), Arrays.toString(answerArray)));
 
         List<Integer> distinctList = new ArrayList<>();
@@ -48,13 +48,20 @@ public class RemoveDuplicatesFromSortedArray implements Test {
             counter += totalSameValueItems;
         }
 
-        int[] distinctArray = ArrayUtils.toPrimitive(distinctList.toArray(new Integer[0]));
+//        int[] distinctArray = ArrayUtils.toPrimitive(distinctList.toArray(new Integer[0]));
+//        for leet code
+        int[] distinctArray = new int[distinctList.size()];
+        int counterDistinctArray = 0;
+
+        for(int number : distinctList) {
+            distinctArray[counterDistinctArray++] = number;
+        }
 
         Printer.print(MessageFormat.format("Answer Array: {0} | {1}", Arrays.toString(distinctArray), (Arrays.toString(distinctArray).equals(Arrays.toString(answerArray)))));
         Printer.print(MessageFormat.format("Answer Count: {0} | {1}", distinctArray.length, (distinctArray.length == answerArray.length)));
 
-        result.setValue(distinctArray.length == answerArray.length);
+        result.setValue(distinctArray.length == answerArray.length && (Arrays.toString(distinctArray).equals(Arrays.toString(answerArray))));
 
-        return distinctArray;
+        return distinctArray.length;
     }
 }
